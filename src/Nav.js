b6906e45f83b2a3c641d91a8b77e90abeba429fe
domzalex <alexdomzalski@gmail.com>
2021-08-24 23:03:13 -0400

    minor styling tweaks

--- src/Nav.js
+++ src/Nav.js
@@ -7,7 +7,7 @@ function Nav() {
         <div>
           <nav>
             <Link to="/About" className="nav-link"><li>about</li></Link>
-            <a href="http://theculdesac.club/" rel="noreferrer" className="nav-link"target="_blank">blog</a>
+            <a href="https://theculdesac.club/" rel="noreferrer" className="nav-link"target="_blank">blog</a>
             <Link to="/" className="homeLink"><li>Alex<br />Domzalski</li></Link>
             <a href="https://github.com/domzalex" rel="noreferrer" className="nav-link" target="_blank">github</a>
             <Link to="/Projects" className="nav-link"><li>projects</li></Link>
@@ -16,7 +16,7 @@ function Nav() {
             <Link to="/" className="homeLink"><li>Alex Domzalski</li></Link>
             <div className="mobile-links">
             <Link to="/About" className="nav-link"><li>about</li></Link>
-            <a href="http://theculdesac.club/" rel="noreferrer" className="nav-link"target="_blank">blog</a>
+            <a href="https://theculdesac.club/" rel="noreferrer" className="nav-link"target="_blank">blog</a>
             <a href="https://github.com/domzalex" rel="noreferrer" className="nav-link" target="_blank">github</a>
             <Link to="/Projects" className="nav-link"><li>projects</li></Link>
             </div>
